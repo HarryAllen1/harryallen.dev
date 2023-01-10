ce05e4b4cd49955b489d60dee3c23f3736092b49
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-01-09 21:20:20 -0800

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import '@unocss/reset/tailwind.css';
+	import { onMount } from 'svelte';
 	import 'uno.css';
 	import './theme.scss';
+
+	onMount(async () => {
+		if (browser) {
+			const { inject } = await import('@vercel/analytics');
+			inject();
+		}
+	});
 </script>
 
 <div class="prose max-w-full h-full">
