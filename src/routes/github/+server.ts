39fe48b75352f72078362e02537a83d31c20f316
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-03-27 23:32:53 -0700

    what was i thinking

--- src/routes/github/+server.ts
+++ src/routes/github/+server.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST = (() => {
+export const GET = (() => {
 	throw redirect(301, 'https://github.com/HarryAllen1');
 }) satisfies RequestHandler;
