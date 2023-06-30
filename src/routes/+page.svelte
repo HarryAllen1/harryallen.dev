3e13979f2354b98a10e0746ab6bab8a18aefe021
Nathan Feiler <79991107+Pyro569@users.noreply.github.com>
2023-06-30 14:12:16 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,7 @@
 </svelte:head>
 
 <div class="w-full flex flex-col items-start pl-[30%] justify-center h-full">
-	<h1 class="text-transparent bg-clip-text bg-gradient-radial from-white to-[#77C] w-fit">
-		Harry Allen
-	</h1>
+	<h1 class="text-transparent bg-clip-text bg-gradient-radial from-white to-[#77C] w-fit">Harry Allen</h1>
 	<h2 class="mt-2">Links</h2>
 	<div>
 		<a href="/github">GitHub</a>
