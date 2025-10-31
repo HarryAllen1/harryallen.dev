3af932661746bbfa23bc3fdfa2016d97d2aca71e
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-10-31 16:38:32 +0000

    make font flash slightly less jarring, add some animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,4 +13,25 @@
 	<title>Harry Allen</title>
 </svelte:head>
 
-{@render children?.()}
+<main>
+	{@render children?.()}
+</main>
+
+<style>
+	@keyframes fade-in {
+		from {
+			transform: translateY(-5px);
+			opacity: 0;
+		}
+		to {
+			transform: translateY(0);
+			opacity: 1;
+		}
+	}
+
+	@media (prefers-reduced-motion: no-preference) {
+		main {
+			animation: fade-in normal 0.5s;
+		}
+	}
+</style>
